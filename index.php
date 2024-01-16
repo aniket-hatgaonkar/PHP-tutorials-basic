50c0b5563a4bb4e6242447308c3eeea8ed53b48a
Aniket Hatgaonkar <115136289+aniket-hatgaonkar@users.noreply.github.com>
2024-01-16 13:42:43 +0530

    v1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- Start PHP Tag -->
+<?php
+    //Single line comment
+    # Single line comment
+    
+    $name = 'Aniket Hatgaonkar';
+    $address ='Nagpur';
+
+    echo $name;
+    echo '<br>'; // '' ==> String
+    echo "<br>"; // "" ==> String
+    echo $address;
+    /* 
+    These is basic multiline comment 
+    write anything  inside these comment
+    tag 
+    etc.
+    */
+?>
+<?php
+
+
+?>
+<!-- End PHP Tag  -->
