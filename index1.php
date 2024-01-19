70e01e7f50af768511c0486de7c6af84dd9fb575
Aniket Hatgaonkar <115136289+aniket-hatgaonkar@users.noreply.github.com>
2024-01-19 16:28:36 +0530

    v1

---
+++ index1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Tutorial part 3</title>
+        <style>
+            /* ----------CSS Selector------------ */
+            h1{
+                /* 1. Element Selector  */
+                color:red;
+            }
+        </style>
+    </head>
+<body>
+    <?php
+        #-----------PHP Tag----------
+        echo 'Welcomme to PHP Traning';
+        echo "<h1>ANIKET</h1>";
+        echo '<h2>AKASH<h2>';
+
+        $x = 10;
+        echo $X;
+    ?>
+    <script>
+        //----------JS tag-----------
+        document.querySelector('h2').style.color= 'green';
+    </script>
+</body>
+</html>
+<?php
+
+?>
